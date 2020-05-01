da6c04f5c39643af57569fdfcb88ad65eadbe4e7
Darth9 <reyprimerjefe@gmail.com>
2020-05-01 18:51:18 +0100

    Update kernel and tidy up tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,60 +1,54 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2018-2020 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-# Copyright (C) 2019-2020 OrangeFox Recovery Project
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="sagit"
-#OF_SUPPORT_SAR="1"
-
-Configure_OF_SAR_Settings() {
-local SCRIPTNAME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-local SCRIPTPATH=$(dirname $SCRIPTNAME)
-local FF_FSTAB=$SCRIPTPATH/recovery.fstab
-local FF_RC=$SCRIPTPATH/recovery/root/init.recovery.qcom.rc
-local TAG="#disabled=#"
-local SYS_ROOT="/system_root"
-   if [ -f $FF_RC ] && [ -f $FF_FSTAB ]; then
-   	local F=$(grep "$TAG" $FF_FSTAB)
-   	local G=$(grep "$TAG" $FF_RC)
-   	if [ "$OF_SUPPORT_SAR" = "1" ]; then
-   	   export BOARD_BUILD_SYSTEM_ROOT_IMAGE="true"
-   	   export OF_SUPPORT_PRE_FLASH_SCRIPT="1"  
-	   [ -n "$F" ] && sed -i -e "s|$TAG||" $FF_FSTAB
-	   [ -n "$G" ] && sed -i -e "s|$TAG||" $FF_RC
-   	else
-	   [ -z "$F" ] && sed -i -e 's|'"$SYS_ROOT"' ext4 |'"$TAG"''"$SYS_ROOT"' ext4 |' $FF_FSTAB
-	   [ -z "$G" ] && sed -i -e 's|export ANDROID_ROOT '"$SYS_ROOT"'|'"$TAG"'export ANDROID_ROOT '"$SYS_ROOT"'|' $FF_RC  
-   	fi
-   fi
-}
-
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export PLATFORM_VERSION=10.0.0
+   	export PLATFORM_VERSION="16.1.0"
    	export PLATFORM_SECURITY_PATCH="2099-12-31"
+	export TW_DEFAULT_LANGUAGE="en"
    	export OF_OTA_RES_DECRYPT=1
    	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-   	export FOX_REPLACE_BUSYBOX_PS=1
-   	export FOX_USE_BASH_SHELL=1
-   	export FOX_USE_NANO_EDITOR=1
    	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+ 	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+   	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+   	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_ZIP_BINARY=1
+	export OF_FLASHLIGHT_ENABLE=0
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+        export OF_KEEP_DM_VERITY="1"  # enabled for R10.1
+	export OF_DISABLE_FORCED_ENCRYPTION="1"  # enabled for R10.1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 
-#   	Configure_OF_SAR_Settings   
-
-add_lunch_combo omni_"$FOX_BUILD_DEVICE"-eng
-add_lunch_combo omni_"$FOX_BUILD_DEVICE"-userdebug
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+  	
+	add_lunch_combo omni_"$FDEVICE"-eng
+	add_lunch_combo omni_"$FDEVICE"-userdebug
 fi
 #
